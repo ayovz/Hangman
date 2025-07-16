f9bef9a7a81f8358e9852f992918e6453d7b5b2a
Ayomal Weerasinghe <65837792+ayovz@users.noreply.github.com>
2025-07-16 11:16:48 +0000

    wordbase added

---
+++ hangman_words.py
@@ -0,0 +1,26 @@
+word_list = [
+  # Animals
+  "elephant", "giraffe", "kangaroo", "penguin", "dolphin",
+  "rhinoceros", "octopus", "cheetah", "flamingo", "chameleon",
+
+  # Fruits
+  "watermelon", "strawberry", "blueberry", "pineapple", "raspberry",
+  "blackberry", "pomegranate", "kiwifruit", "dragonfruit", "passionfruit",
+
+  # Countries
+  "canada", "brazil", "australia", "japan", "germany",
+  "argentina", "ethiopia", "indonesia", "philippines", "madagascar",
+
+  # Sports
+  "basketball", "volleyball", "badminton", "wrestling", "gymnastics",
+  "skateboarding", "swimming", "athletics", "archery", "fencing",
+
+  # Professions
+  "engineer", "architect", "physician", "pharmacist", "astronomer",
+  "veterinarian", "journalist", "programmer", "electrician", "mechanic",
+
+  # Random
+  "sunflower", "universe", "adventure", "mystery", "whisper",
+  "jigsaw", "zigzag", "keyboard", "bookworm", "nightmare"
+]
+
